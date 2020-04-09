1d7699d038d6ecf0ba170e6c3b0684fd4ab7e109
mmkaruiru <mmkaruiru@hotmail.com>
2020-04-09 15:13:20 +0300

    Loading the Dataset from web to RStudio

---
+++ unVoting.R
@@ -0,0 +1,7 @@
+# Importation of Dataset(United Nations Voting Dataset)
+#Loading RDS file from website.
+#First Run the file through a built in decomposser function (gzcon)
+#Then load it through RBase function readRDS
+
+unData =readRDS(gzcon(url("https://assets.datacamp.com/production/repositories/420/datasets/ddfa750d993c73026f621376f3c187f276bf0e2a/votes.rds")))
+head(unData)
