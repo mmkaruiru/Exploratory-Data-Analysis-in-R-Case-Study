4003887f62bfd4a0288d75053f7e83bfc860126a
mmkaruiru <mmkaruiru@hotmail.com>
2020-04-24 16:31:40 +0300

    "Adding Columns and Filtering"

--- unVoting.R
+++ unVoting.R
@@ -1,4 +1,4 @@
-# Importation of Dataset(United Nations Voting Dataset)
+# Importation of Dataset(United Nations Voting in General Assembly)
 #Loading RDS file from website.
 #First Run the file through a built in decomposser function (gzcon)
 #Then load it through RBase function readRDS
@@ -8,4 +8,35 @@ unData =readRDS(gzcon(url("https://assets.datacamp.com/production/repositories/4
 #Observing firs 5 rows of the dataset
 head(unData)
 
+#Variables names explanations
+#rcid -Roll call ID- Describes one round of voting to approve UN resolution
+#session - Represents the year long session when the vote was cast
+#vote - Represents a country's choice. eg 1 means Yes, 9 - Country not a memeber of UN
+#ccode - Country code, uniquely specifies the country
+# Importing dplyr
+library(dplyr)
 
+#Filtering using pipe operator
+# On vote variable we only care of 1(yes), 2(no), 3(abstain)
+unData%>%
+
+#Mutate year to since the session was after 1945
+unData%>%mutate(year = session + 1945)
+
+#The ccode represents  "Correlates of Warcodes" which are in countrycode package
+library(countrycode)
+#Getting the country names from the package, example
+countrycode(2 , "cown", "country.name")
+
+#Mutating country name corresponding with the ccode
+unData%>%mutate(countryname = countrycode(ccode, "cown","country.name"))
+
+#Combining the whole code assigning to unData_Processed
+unData_Processsed <-unData%>%filter(vote <= 3)%>% 
+mutate(countryname = countrycode(ccode, "cown","country.name"), year = session + 1945)
+unData_Processsed
+
+
+
+
+                                       
